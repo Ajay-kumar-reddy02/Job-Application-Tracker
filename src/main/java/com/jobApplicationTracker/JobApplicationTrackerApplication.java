af78c7235c8cb6c9be2eeca1d0b72cde4b283623
Ajay <ajaykumar.kondreddy02@gmail.com>
2024-11-21 21:50:24 +0530

    Sysout Updated

--- src/main/java/com/jobApplicationTracker/JobApplicationTrackerApplication.java
+++ src/main/java/com/jobApplicationTracker/JobApplicationTrackerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobApplicationTrackerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JobApplicationTrackerApplication.class, args);
+		System.out.println("------");
 	}
 
 }
